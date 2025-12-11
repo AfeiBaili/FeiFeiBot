90177206cbdb2d8407e839e04f09db2362850e63
Afei <afeibaili@outlook.com>
2025-12-11 20:40:55 +0800

    消息协程化

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
 }
 
 group = "online.afeibaili"
-version = "3.14.0"
+version = "3.14.1"
 
 repositories {
     maven("https://maven.aliyun.com/repository/public")
--- src/main/kotlin/online/afeibaili/FeiFeiBot.kt
+++ src/main/kotlin/online/afeibaili/FeiFeiBot.kt
@@ -15,7 +15,7 @@ object FeiFeiBot : KotlinPlugin(
     JvmPluginDescription(
         id = "online.afeibaili.feifeibot",
         name = "FeiFeiBot",
-        version = "3.14.0",
+        version = "3.14.1",
     ) {
         author("AfeiBaili")
     }) {
